74f6f1c4274d0f348cdb2a23aa9c0488e9381bc5
Domingo Wilman Cabral <38302824+dwcabral@users.noreply.github.com>
2018-04-13 17:44:43 -0400

    Primera Carga

---
+++ ProyectoExamenFinal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, resultado;
+            string nombre = "DOMINGO CABRAL,FRANCIS MONTERO,YSUAL MONTERO";
+            string matricula = "08-EISN-032,13-EISM-1-021,13-EISM-1-035";
+
+            Console.WriteLine("Escriba el primer numero:");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba el segundo numero:");
+            numero2 = int.Parse(Console.ReadLine());
+
+            resultado = numero1 + numero2;
+
+            while (resultado == 2500)
+            {
+                Console.WriteLine("La suma es :" + resultado);
+            }
+
+            for (int i = 0; i < numero1; i++)
+            {
+                Console.WriteLine("La suma es :" + resultado);
+            }
+            if (nombre == "DOMINGO CABRAL,FRANCIS MONTERO,YSUAL MONTERO")
+            {
+                Console.WriteLine("Nombre :" + nombre);
+            }
+            if (matricula == "08-EISN-032,13-EISM-1-021,13-EISM-1-035")
+            {
+                Console.WriteLine("Matricula :" + matricula);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
